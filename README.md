1f16105756ae24b888993f4d1568ba5158794dd3
quinton22 <quintonhoffman22@gmail.com>
2018-05-22 17:05:58 -0500

    Initial commit: basic flutter app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Coordn8r
+# coordn8r
+
+Application to facilitate all aspects of teamwork, collaboration, and coordination.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ coordn8r/android/app/src/main/java/coordn8r/github/quinton22/coordn8r/MainActivity.java
@@ -0,0 +1,14 @@
+package coordn8r.github.quinton22.coordn8r;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
