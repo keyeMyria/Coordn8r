7ec92e357d5b0ce8f39ec900ec1120f4d1173e7a
quinton22 <quintonhoffman22@gmail.com>
2018-07-12 21:45:29 -0500

    Fixed parenthetical error & changed name from coordn8r to Coordn8r

--- coordn8r/lib/pages/home_page.dart
+++ coordn8r/lib/pages/home_page.dart
@@ -11,92 +11,90 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new DefaultTabController(
-        length: 4,
-        child: new Scaffold(
-          appBar: new AppBar(
-            leading: new Padding(
-              padding: EdgeInsets.all(8.0),
-              child: InkWell(
-                child: Image(
-                  image: AssetImage("assets/logo/logo96.png"),
-                ),
-                //TODO make this trigger dialog asking whether or not we are sure
-                onTap: () => showDialog(
-                      context: context,
-//          barrierDismissible: false, // must press button
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text('Are you sure?'),
-                          content: Text('Going back will log you out.'),
-                          actions: <Widget>[
-                            FlatButton(
-                              onPressed: () => Navigator.of(context).pushNamed(
-                                  LoginPage
-                                      .tag), // also Navigator.of(context).pop(true)
-                              child: Text('Yes'),
-                            ),
-                            FlatButton(
-                              onPressed: () => Navigator.of(context).pop(false),
-                              child: Text('No'),
-                            )
-                          ],
-                        );
-                      },
-                    ),
+      length: 4,
+      child: new Scaffold(
+        appBar: new AppBar(
+          leading: new Padding(
+            padding: EdgeInsets.all(8.0),
+            child: InkWell(
+              child: Image(
+                image: AssetImage("assets/logo/logo96.png"),
               ),
-            ),
-            title: new Text('COORDN8R'),
-            actions: <Widget>[
-              new IconButton(
-                  icon: new Icon(Icons.settings_applications),
-                  iconSize: 40.0,
-                  color: Colors.white,
-                  //TODO set onPressed to open up a menu that lets you log out
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed(LoginPage.tag))
-            ],
-          ),
-          body: new SafeArea(
-            child: new TabBarView(
-              children: [
-                new TeamsPage(),
-                new Icon(
-                  Icons.calendar_today,
-                  color: Theme.of(context).primaryColor,
-                ),
-                new Icon(
-                  Icons.fiber_new,
-                  color: Theme.of(context).primaryColor,
-                ),
-                new Icon(
-                  Icons.account_box,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ],
+              //TODO make this trigger dialog asking whether or not we are sure
+              onTap: () => showDialog(
+                    context: context,
+//          barrierDismissible: false, // must press button
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Are you sure?'),
+                        content: Text('Going back will log you out.'),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () =>
+                                Navigator.of(context).pushNamed(LoginPage.tag),
+                            // also Navigator.of(context).pop(true)
+                            child: Text('Yes'),
+                          ),
+                          FlatButton(
+                            onPressed: () => Navigator.of(context).pop(false),
+                            child: Text('No'),
+                          )
+                        ],
+                      );
+                    },
+                  ),
             ),
           ),
-          bottomNavigationBar: new TabBar(
-            labelColor: Theme.of(context).indicatorColor,
-            unselectedLabelColor: Color(0xFF74542A),
-            tabs: [
-              new Tab(
-                  icon: new Icon(
-                CommunityMaterialIcons.group,
-              )),
-              new Tab(
-                  icon: new Icon(
+          title: new Text('COORDN8R'),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.settings_applications),
+                iconSize: 40.0,
+                color: Colors.white,
+                //TODO set onPressed to open up a menu that lets you log out
+                onPressed: () => Navigator.of(context).pushNamed(LoginPage.tag))
+          ],
+        ),
+        body: new SafeArea(
+          child: new TabBarView(
+            children: [
+              new TeamsPage(),
+              new Icon(
                 Icons.calendar_today,
-              )),
-              new Tab(
-                  icon: new Icon(
+                color: Theme.of(context).primaryColor,
+              ),
+              new Icon(
                 Icons.fiber_new,
-              )),
-              new Tab(
-                  icon: new Icon(
+                color: Theme.of(context).primaryColor,
+              ),
+              new Icon(
                 Icons.account_box,
-              )),
+                color: Theme.of(context).primaryColor,
+              ),
             ],
           ),
+        ),
+        bottomNavigationBar: new TabBar(
+          labelColor: Theme.of(context).indicatorColor,
+          unselectedLabelColor: Color(0xFF74542A),
+          tabs: [
+            new Tab(
+                icon: new Icon(
+              CommunityMaterialIcons.group,
+            )),
+            new Tab(
+                icon: new Icon(
+              Icons.calendar_today,
+            )),
+            new Tab(
+                icon: new Icon(
+              Icons.fiber_new,
+            )),
+            new Tab(
+                icon: new Icon(
+              Icons.account_box,
+            )),
+          ],
         ),
       ),
     );
